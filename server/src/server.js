8918b36b0e60145c493ccf209fada20a9e965e9a
Aleem Awan <muhammadaleem366@gmail.com>
2022-05-28 09:59:19 +0500

    connects to  mongo database

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,27 @@
 const http = require("http");
+const mongoose = require("mongoose");
 const app = require("./app");
 const { loadPlanetsData } = require("../models/planets.model");
 const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 
 // server setup
-const MONGO_URL = "http://localhost:27017/nasa";
+const MONGO_URL = "mongodb://localhost:27017/nasa";
+
+// use once if you are sure that event will be emitted once
+
+mongoose.connection.once("open", () => {
+  console.log("connected to mongoose");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
 
 async function startServer() {
+  mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+  });
   await loadPlanetsData();
   server.listen(PORT, () => {
     console.log(`Server is listening to the port ${PORT}`);
